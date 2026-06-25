215ee85598e022174eaa4c0d35b83e4a8fb9bf82
tunnaung2000-coder <tunnaung2000@gmail.com>
2026-06-25 23:28:43 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "sp-2in1-scanner-v3";
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => {
+      if (key !== CACHE_NAME) return caches.delete(key);
+    }))).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  const req = event.request;
+  if (req.method !== "GET") return;
+
+  event.respondWith(
+    fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(req, copy));
+      return res;
+    }).catch(() => caches.match(req).then(cached => cached || caches.match("./index.html")))
+  );
+});
